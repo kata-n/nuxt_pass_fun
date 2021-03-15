694ac8cacc2a7ec9723cc2f80b35a1de81dd9ff0
河村 泰佑 <kata.kawa813@gmail.com>
2021-03-15 19:27:56 +0900

    add

--- components/InputmakingBox.vue
+++ components/InputmakingBox.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <input
-      class="w-px flex-1 border h-10 bg-gray-200 text-gray-700 border border-gray-200 rounded rounded-r-none py-3 px-4 focus:outline-none focus:bg-white focus:border-gray-500"
-      type="text"
-    />
+    <input class="input__box" type="text" />
+    <p>テスト</p>
   </div>
 </template>
 
 <script lang="ts"></script>
 
-<style></style>
+<style>
+.input__box {
+  padding: 10px;
+}
+</style>
