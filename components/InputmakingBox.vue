e5cdf318a6765620f04efd0c9c6480277b4e27fe
河村 泰佑 <kata.kawa813@gmail.com>
2021-03-19 20:10:11 +0900

    add

--- components/InputmakingBox.vue
+++ components/InputmakingBox.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <input class="input__box" v-bind:type="textPass" />
-    <button type="button" @click="chageVisible">表示切り替え</button>
+    <input class="input__box" v-bind:type="textPass" ref="passwordStrong" />
+    <br />
+    <button type="button" class="show__btn" @click="chageVisible">
+      {{ showBtnname }}
+    </button>
+    <div>{{ textPass.length }}</div>
   </div>
 </template>
 
@@ -14,21 +18,29 @@ export default Vue.extend({
   data() {
     return {
       textPass: "text",
-      showPassword: false
+      showPassword: false,
+      showBtnname: "隠す"
     };
   },
   methods: {
     chageVisible: function() {
       this.showPassword = !this.showPassword;
       this.textPass = this.showPassword ? "text" : "password";
+      this.showBtnname = this.showPassword ? "隠す" : "表示する";
     }
-  }
+  },
+  computed: {}
 });
 </script>
 
 <style>
 .input__box {
   margin-top: 10px;
+  padding: 10px;
+}
+.show__btn {
+  margin: 20px;
+  width: 140px;
   padding: 10px;
 }
 </style>
