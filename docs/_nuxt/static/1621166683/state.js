5cf6574d392944cc5887c6ab4996819d22d158ea
河村 泰佑 <kata.kawa813@gmail.com>
2021-05-16 21:07:59 +0900

    add

--- docs/_nuxt/static/1621166683/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_pass_fun\u002F_nuxt\u002Fstatic\u002F1621166683",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fnuxt_pass_fun\u002F",assetsPath:"\u002Fnuxt_pass_fun\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6ea8db30"}}}}(null));
