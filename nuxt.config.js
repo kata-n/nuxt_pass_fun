06dd5837a5e6fd99451cf0aacbc698dad6cae151
河村 泰佑 <kata.kawa813@gmail.com>
2021-05-16 19:41:24 +0900

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,8 @@ export default {
       {
         rel: "icon",
         type: "image/x-icon",
-        href: "/nuxt_pass_fun/favicon.ico"
+        href:
+          "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🍣</text></svg>"
       }
     ]
   },
